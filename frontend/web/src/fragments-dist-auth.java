e1110754739664fd759e38d1f7a7c41c9263052b
annnk8hender4 <d_salnikov_761394090@rambler.ru>
2025-12-05 05:20:06 +0800

    add ZamaOracleAddress.sol, App.css, solidity-files-cache.json

---
+++ frontend/web/src/fragments-dist-auth.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\fragments-dist-auth.java
+// Generated at: 2025-10-20T08:21:33.094Z
---
+++ frontend/web/src/handler-order.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\handler-order.rs
+// Generated at: 2025-10-20T08:21:33.103Z
